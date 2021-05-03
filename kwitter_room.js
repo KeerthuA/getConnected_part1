058a8a1a07ff8533d4701cbaa22673cad847080c
KeerthuA <77785377+KeerthuA@users.noreply.github.com>
2021-05-03 10:51:28 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+// Your web app's Firebase configuration
+var firebaseConfig = {
+      apiKey: "AIzaSyDz63_jtWKQYiwfTpXyUpLYf0JDu4ePMUE",
+      authDomain: "project1-30a45.firebaseapp.com",
+      databaseURL: "https://project1-30a45-default-rtdb.firebaseio.com",
+      projectId: "project1-30a45",
+      storageBucket: "project1-30a45.appspot.com",
+      messagingSenderId: "638048537108",
+      appId: "1:638048537108:web:54a1e2854642d7719f7501"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
